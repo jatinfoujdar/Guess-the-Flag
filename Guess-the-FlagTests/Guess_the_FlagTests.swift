780a35134390aae4182f9a163dd4251434152d9c
Jatin Foujdar <jatin321001@gmail.com>
2024-10-04 22:06:09 +0530

    Initial Commit

---
+++ Guess-the-FlagTests/Guess_the_FlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagTests.swift
+//  Guess-the-FlagTests
+//
+//  Created by jatin foujdar on 04/10/24.
+//
+
+import Testing
+@testable import Guess_the_Flag
+
+struct Guess_the_FlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
