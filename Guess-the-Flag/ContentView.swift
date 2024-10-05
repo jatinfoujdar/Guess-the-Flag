1d9f21337275889c8f2ee3ac3bf09fdba9deba4d
Jatin Foujdar <jatin321001@gmail.com>
2024-10-05 23:39:58 +0530

    assest commit

--- Guess-the-Flag/ContentView.swift
+++ Guess-the-Flag/ContentView.swift
@@ -9,26 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete Selection",role: .destructive, action: executeDelete)
-    }
-    func executeDelete(){
-        
-    }
-        //        LinearGradient(stops: [
-        //            .init(color: .white,location: 0.45),
-        //            .init(color: .black,location: 0.55)
-        //        ], startPoint: .top, endPoint: .bottom)
-        //        ZStack{
-        //            VStack(spacing: 0){
-        //                Color.red
-        //                Color.blue
-        //            }
-        //            Text("Your content")
-        //                .foregroundStyle(.secondary)
-        //                .padding(50)
-        //                .background(.ultraThinMaterial)
-        //        }
-        //        .ignoresSafeArea()
+ 
 
 }
 
